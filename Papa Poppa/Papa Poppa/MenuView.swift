bb6e8b119643b58337ddf42068faae5cba79c103
Annika <aawiesinger@gmail.com>
2017-04-17 12:44:07 -0400

    yay now there's two buttons!

--- Papa Poppa/Papa Poppa/MenuView.swift
+++ Papa Poppa/Papa Poppa/MenuView.swift
@@ -13,8 +13,8 @@ import SpriteKit
 
 class MenuView: UIView {
     var vc: GameViewController?
-    var label: UILabel!
-    var button: UIButton!
+    var nextLevelButton: UIButton!
+    var retryLevelButton: UIButton!
 
     override init(frame: CGRect) { // for using CustomView in code
         super.init(frame: frame)
@@ -32,17 +32,35 @@ class MenuView: UIView {
         //layer.cornerRadius = frame.width / 2
         self.backgroundColor = UIColor.green
         //label.setTitle("yo", for: UIControlState.normal)
-        label = UILabel(frame: CGRect(x: 12, y: 8, width: self.frame.size.width-90, height: 50))
-        label.text = "Connection error please try again later!!"
-        label.textColor = UIColor.white
-        label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 14)
-        self.addSubview(label)
+//        label = UILabel(frame: CGRect(x: 12, y: 8, width: self.frame.size.width-90, height: 50))
+//        label.text = "Retry level"
+//        label.textColor = UIColor.white
+//        label.numberOfLines = 0
+//        label.font = UIFont.systemFont(ofSize: 14)
+//        self.addSubview(label)
         
-        button = UIButton(frame: CGRect(x: self.frame.size.width-87, y: 8, width: 86, height: 50))
-        button.setTitle("OK", for: UIControlState.normal)
-        button.setTitleColor(UIColor(red: 76/255, green: 175/255, blue: 80/255, alpha: 1.0), for: UIControlState.normal)
-        button.setTitleColor(UIColor.red, for:UIControlState.highlighted)
-        self.addSubview(button)
+        nextLevelButton = UIButton(frame: CGRect(x: self.frame.size.width-87, y: 8, width: 86, height: 50))
+        nextLevelButton.setTitle("Next Level", for: UIControlState.normal)
+        nextLevelButton.setTitleColor(UIColor(red: 76/255, green: 175/255, blue: 80/255, alpha: 1.0), for: UIControlState.normal)
+        nextLevelButton.addTarget(self, action: #selector(MenuView.nextLevel(_:)), for: UIControlEvents.touchDown)
+        self.addSubview(nextLevelButton)
+        
+        retryLevelButton = UIButton(frame: CGRect(x: self.frame.size.width-87, y: 8, width: 86, height: 50))
+        retryLevelButton.setTitle("Retry Level", for: UIControlState.normal)
+        retryLevelButton.setTitleColor(UIColor(red: 76/255, green: 175/255, blue: 80/255, alpha: 1.0), for: UIControlState.normal)
+        retryLevelButton.addTarget(self, action: #selector(MenuView.retryLevel(_:)), for: UIControlEvents.touchDown)
+        self.addSubview(retryLevelButton)
+    }
+    
+    func nextLevel(_ sender: UITapGestureRecognizer) {
+        print("switchLevelButtonPressed")
+        nextLevelButton.setTitleColor(UIColor.red, for: UIControlState.normal)
+//        removeFromSuperview()
+    }
+    
+    func retryLevel(_ sender: UITapGestureRecognizer) {
+        print("nextLevelButtonPressed")
+        nextLevelButton.setTitleColor(UIColor.red, for: UIControlState.normal)
+        //        removeFromSuperview()
     }
 }
